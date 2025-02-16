0c04b9c6114e3d3071f6b51066df648ca39606d3
Yorb1431 <yorbevandermast@gmail.com>
2025-02-16 21:19:09 +0100

    Headers API.PHP

--- backend/API/API.php
+++ backend/API/API.php
@@ -1,5 +1,8 @@
 <?php
-
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Methods: POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
 
 
 
