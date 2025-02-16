a24e79dd8b360f291b98f74640c017f357063d86
Yorb1431 <yorbevandermast@gmail.com>
2025-02-16 21:31:40 +0100

    Headers an.php

--- backend/API/analyze.php
+++ backend/API/analyze.php
@@ -1,5 +1,9 @@
 <?php
-
+// Headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 
 ?>
